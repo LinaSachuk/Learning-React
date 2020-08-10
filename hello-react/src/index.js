7b263661554c2c0b3db22d364d37e269fdc210c5
Lina Sachuk <sachuklina@gmail.com>
2020-08-10 06:29:06 -0600

    add useEffect

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,34 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
 
-function App() {
-  const [manager, setManager] = useState('Alex');
-  const [status, setStatus] = useState('Open');
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    alert(`checked: ${checked.toString()}`);
+  })
   return (
     <>
-      <div>
-        <h1>Manager on Duty: {manager}</h1>
-        <button onClick={() => setManager('Rachel')}>New Manager</button>
-      </div>
-      <div>
-        <h1>Status: {status}</h1>
-        <button onClick={() => setStatus('Open')}>Open</button>
-        <button onClick={() => setStatus('Break')}>Back in 5</button>
-        <button onClick={() => setStatus('Closed')}>Closed</button>
-
-      </div>
+      <input type="checkbox"
+        value={checked}
+        onChange={() =>
+          setChecked(checked => !checked)
+        }
+      />
+      {checked ? 'checked' : 'not checked'}
     </>
-  );
+  )
 }
 
 
 
-
 ReactDOM.render(
-  <App
+  <Checkbox
   />,
   document.getElementById('root')
 );
