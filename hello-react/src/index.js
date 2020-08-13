72fd8dba1d7615152b234ccf19e6d00ea86daec4
Lina Sachuk <sachuklina@gmail.com>
2020-08-12 23:00:05 -0600

    add compose components

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+const Book = ({ title, author, pages }) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p> Pages: {pages}</p>
+    </section>
+  )
+}
+
+
+
 const Library = () => {
   return (
     <div>
-      Welcome to the Library!
+      <Book title="The sun also rises" author="Ernest Hemingway" pages={260} />
+      <Book title="Memories of my melancholy whores" author="Gabriel Garcia Marquez" pages={360} />
+      <Book title="Hi hi" author="Hemingway" pages={240} />
+
     </div>
   )
 }
@@ -15,10 +30,6 @@ const Library = () => {
 
 
 render(
-  <div>
-    <Library />
-    <Library />
-  </div>
-  ,
+  <Library />,
   document.getElementById('root')
 )
